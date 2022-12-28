9f82409f72d3cf9177b7030b565cc5cc0b2f813e
Gennerentola <gennaro.notaro1@gmail.com>
2022-12-28 11:28:14 +0100

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AuthComponent } from './components/auth/auth.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "auth",
+    component: AuthComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/interfaces/event.interface.ts
@@ -0,0 +1,10 @@
+export interface Event {
+  "id": number,
+  "name": string,
+  "sport": string,
+  "gender": string,
+  "place": string,
+  "rules": string,
+  "date": string,
+  "organizer": string
+}
